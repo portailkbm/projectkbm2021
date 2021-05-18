9f966feed14284d98a6136446fd46905a48b32c0
blacol <anthonycolvil@gmail.com>
2021-05-18 14:44:27 +0200

    ajout code évènement

---
+++ code projet ici/Projet_KBM/Sprint 1 et 2/modelevenement.php
@@ -0,0 +1,75 @@
+<?php
+
+    function visualiser()
+    {
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=alimentaire;charset=utf8', 'root', '');
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+
+        $requete = $bdd->prepare('SELECT * FROM evenement WHERE visevent = 1');
+        $requete ->execute();
+
+        if ( $requete ->execute())
+        {
+            while($donnee = $requete->fetch())
+            {
+                echo ' id = '.$donnee["idevent"].'</br>';
+                echo ' type = '.$donnee["typeevent"].'</br>';
+                echo ' lib = '.$donnee["libevent"].'</br>';
+                echo ' des = '.$donnee["desevent"].'</br>';
+                echo ' photo = '.$donnee["photoevent"].'</br>';
+                echo ' flyer = '.$donnee["flyer"].'</br>';
+                echo ' debut = '.$donnee["debutevent"].'</br>';
+                echo ' debut_hr = '.$donnee["debutevent_hr"].'</br>';
+                echo ' fin = '.$donnee["finevent"].'</br>';
+                echo ' fin_fr = '.$donnee["finevent_hr"].'</br>';
+                echo ' cr_event = '.$donnee["cr_event"].'</br>';
+                echo ' visevent = '.$donnee["visevent"].'</br>';
+                echo ' code postale = '.$donnee["cp"].'</br>';
+                echo ' idlak = '.$donnee["idlak"].'</br></br>';
+            }
+        }
+    }
+    
+    function filter($trier)
+    {
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=alimentaire;charset=utf8', 'root', '');
+        }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+
+        $requete = $bdd->prepare('SELECT * FROM evenement,lakou  WHERE visevent = 1 ORDER BY `evenement`.`'.$trier.'` ASC' );
+        $requete ->execute();
+
+
+        if ( $requete ->execute())
+        {
+            while($donnee = $requete->fetch())
+            {
+                echo ' id = '.$donnee["idevent"].'</br>';
+                echo ' type = '.$donnee["typeevent"].'</br>';
+                echo ' lib = '.$donnee["libevent"].'</br>';
+                echo ' des = '.$donnee["desevent"].'</br>';
+                echo ' photo = '.$donnee["photoevent"].'</br>';
+                echo ' flyer = '.$donnee["flyer"].'</br>';
+                echo ' debut = '.$donnee["debutevent"].'</br>';
+                echo ' debut_hr = '.$donnee["debutevent_hr"].'</br>';
+                echo ' fin = '.$donnee["finevent"].'</br>';
+                echo ' fin_fr = '.$donnee["finevent_hr"].'</br>';
+                echo ' cr_event = '.$donnee["cr_event"].'</br>';
+                echo ' visevent = '.$donnee["visevent"].'</br>';
+                echo ' code postale = '.$donnee["cp"].'</br>';
+                echo ' idlak = '.$donnee["nomlak"].'</br></br>';
+            }
+        }
+    }
+?>

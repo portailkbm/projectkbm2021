eaac901b0cd528c7b60d2b75ee9db7cdbd9e3d03
blacol <anthonycolvil@gmail.com>
2021-05-19 16:10:33 +0200

    maj

--- code projet ici/Sprint 1 et 2/modelevenement.php
+++ code projet ici/Sprint 1 et 2/modelevenement.php
@@ -4,7 +4,7 @@
     {
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=alimentaire;charset=utf8', 'root', '');
+            $bdd = new PDO('mysql:host=91.216.107.183;dbname=demen1587164;charset=utf8', '5t4fllyba3', '');
         }
         catch (Exception $e)
         {
@@ -40,7 +40,7 @@
     {
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=alimentaire;charset=utf8', 'root', '');
+            $bdd = new PDO('mysql:host=91.216.107.183;dbname=demen1587164;charset=utf8', '5t4fllyba3', '');
         }
         catch (Exception $e)
         {

db37e3ae11bc159aa16aa4e11c15f1de3f14ca17
blacol <anthonycolvil@gmail.com>
2021-06-01 00:12:44 +0200

    finit

--- code projet ici/Sprint 1 et 2/Bdd_login.php
+++ code projet ici/Sprint 1 et 2/Bdd_login.php
@@ -4,11 +4,7 @@
 
 define("BD_USER", "root");
 
-<<<<<<< HEAD
-define("BD_MDP", "5t4fllyba3";
-=======
-define("BD_MDP", "root");
->>>>>>> ee1f866f483f69c22704e1d26c3793e7ae0103d6
+define("BD_MDP", "5t4fllyba3");
 
 define("BD_NAME", "bdd");
 

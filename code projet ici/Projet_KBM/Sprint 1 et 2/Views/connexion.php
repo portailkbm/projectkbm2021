a81d9db00aad225d534269521e8ad40d465c2eac
blacol <anthonycolvil@gmail.com>
2021-07-13 01:48:05 +0200

    ok

--- code projet ici/Projet_KBM/Sprint 1 et 2/Views/connexion.php
+++ code projet ici/Projet_KBM/Sprint 1 et 2/Views/connexion.php
@@ -27,16 +27,16 @@
     }
 ?>
 <!DOCTYPE HTML>
-<html>
+
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="css/style-connexion-main.css?v=<?php echo time() ?>" />
+    <link rel="stylesheet" href="../css/style-connexion-main.css?v=<?php echo time() ?>" />
     <title> KBM - CONNEXION</title>
 </head>
 <body class="orange">
     <div id="en-tete">
         <h1 id="titre"> Connexion </h1>
-       <a href="main.php"> <img id="logo-retour" src="Ressources/logo_retour.png" /> <a/>  
+       <a href="main.php"> <img id="logo-retour" src="../Ressources/logo_retour.png" /> </a>  
     </div>
         <form id="formulaire" method="post" action="connexion_controller.php">
             <div class="element conteneur">

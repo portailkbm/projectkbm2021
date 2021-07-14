ca433a733629adb38c38aa89c58df90db2d9a2b7
biga971 <enlive1.eb@gmail.com>
2021-07-14 07:16:14 -0400

    t

--- code projet ici/Projet_KBM/Sprint 1 et 2/models/Bdd_login.php
+++ code projet ici/Projet_KBM/Sprint 1 et 2/models/Bdd_login.php
@@ -1,15 +1,15 @@
 <?php 
 
 // definition des constantes pour la base de donnée Anthony
-
+/*
 define("BD_USER", "root");
 define("BD_MDP", "root");
-define("BD_NAME", "demen");
+define("BD_NAME", "demen");*/
 
 // definition des constantes pour la base de donnée Enrick
 
 define("BD_USER", "root");
 define("BD_MDP", "");
-define("BD_NAME", "demen");
+define("BD_NAME", "demen1587164");
 
 ?>

9a65d6dad894ab352ef0efea26094ebe88634960
blacol <anthonycolvil@gmail.com>
2021-07-21 15:49:13 +0200

    maj

--- code projet ici/Projet_KBM/Sprint 1 et 2/Views/Producteurs.php
+++ code projet ici/Projet_KBM/Sprint 1 et 2/Views/Producteurs.php
@@ -3,18 +3,44 @@
     <head>
 		<meta charset="utf-8" />
 	   	<link rel="stylesheet" href="../css/style-evenement.css?<?php echo time(); ?>" />
-           
+		<link rel="stylesheet" href="../css/style-produit.css?<?php echo time(); ?>" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		
 		<title>KBM-Produit</title>
 	</head>
-    <body>
-        <div>
-            <h1>Listes des producteurs</h1>
-            <div>
-                <a href="main.php"> <img id="logo-retour" src="../Ressources/logo_retour.png" /> </a>
+    <body style=" background-color: #80B650">
+	<div id="haut">
+		 	<div class="flexheaders">
+				<a href="main.php"> <img id="logo-retour" src="../Ressources/logo_retour.png" /> </a>
+				
+			</div>
+            <div  class="flexheaders2">
+				<h1>Listes des produits</h1>
+                
             </div>
         </div>
+		<div id=divopserstatue></div>
+		<div id="statue">
+			<div>
+				<img src="https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331257__340.png"  width="70" height="80">
+
+			</div>
+			<div>
+				<p>
+					<?php 
+					    session_start();
+						if(isset($_SESSION['pseudo'])){
+							echo $_SESSION['pseudo'];
+						}
+					?>
+				</p>
+				<p>adrresse@mail</p>
+				<div >
+					<button onclick="window.location.href='../controlleur/controleur_deconnexion.php';">Deconnexion</button>
+				</div>
+			</div>
+		</div>
+	</div>
         <br>
 
                 <table class="table">

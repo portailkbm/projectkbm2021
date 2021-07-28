6437585cabe771e3287607ac3c1643f9e4aa336f
blacol <anthonycolvil@gmail.com>
2021-07-28 21:03:18 +0200

    maj

--- code projet ici/Projet_KBM/Sprint 1 et 2/Views/main.php
+++ code projet ici/Projet_KBM/Sprint 1 et 2/Views/main.php
@@ -6,9 +6,7 @@
 		<meta charset="utf-8" />
 		
 		<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-		<link rel="stylesheet" href="../css/style-main.css?v=<?php echo time() ?>" />
-
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="../css/style-main.css?v=<?php echo time()?>" />
 
 		<title>KBM - Acceuile</title>
 	</head>

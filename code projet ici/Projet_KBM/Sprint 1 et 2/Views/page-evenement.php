4dda053420c3b75ff0c2ec409032cf58769d287f
biga971 <enlive1.eb@gmail.com>
2021-08-10 16:45:33 -0400

    r

--- code projet ici/Projet_KBM/Sprint 1 et 2/Views/page-evenement.php
+++ code projet ici/Projet_KBM/Sprint 1 et 2/Views/page-evenement.php
@@ -198,7 +198,9 @@
 			<!-- Le type d'encodage des données, enctype, DOIT être spécifié comme ce qui suit -->
 <form enctype="multipart/form-data" action="../controlleur/test.php" method="post">
   <!-- MAX_FILE_SIZE doit précéder le champ input de type file -->
-  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+  
+ <!-- <input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
+
   <!-- Le nom de l'élément input détermine le nom dans le tableau $_FILES -->
   Envoyez un fichier png : <input name="file" type="file" /> </br>
   Envoyez un fichier pdf : <input name="file2" type="file" /> </br>

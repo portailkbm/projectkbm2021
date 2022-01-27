d2872372774d075b894379a18b5e3d96697a9f4b
S3BVJul <svaurin@gmail.com>
2022-01-26 23:30:58 -0400

    version 1.0.4

--- src/pages/Evenment/index.tsx
+++ src/pages/Evenment/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Event from "./event";
+import Carte from "./cardsEvent";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Accueil = () => {
 
-    return (<div>Nou Kontan vwè'w</div>) 
+    return (<React.Fragment>
+        <div>Nou Kontan vwè'w</div> 
+        <Row>
+				<Col lg="6" xl="5" className="d-flex">
+					<Event/>
+				</Col>
+				<Col lg="6" xl="7" className="d-flex">
+					<Carte/>
+				</Col>
+			</Row>
+        </React.Fragment>) 
     
     }
+
     export default Accueil

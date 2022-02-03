130dd51a17d362405538210c8a3490dbea790e7a
S3BVJul <svaurin@gmail.com>
2022-02-03 15:13:03 -0400

    modif en plus en tant que choix

--- src/pages/Accueil/main.jsx
+++ src/pages/Accueil/main.jsx
@@ -8,4 +8,17 @@ import "./styles.css";
 import App from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-    
+  
+/*
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// eslint-disable-next-line
+import "swiper/css/bundle";
+import "./styles.css";
+
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("app"));
+    */
